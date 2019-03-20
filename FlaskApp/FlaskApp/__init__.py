4ab8e1ef297a4ddf933efd5e60cb7c7807cd0b96
joshuanpedia <joshuanpedia@gmail.com>
2019-03-21 02:29:51 +0900

    더미 파일 삭제 및 render template 컴파일 확인

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, flash, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return "HELL o"
+def homepage():
+	return render_template('index.html')
 
 if(__name__ == 'main'):
 	app.run()
