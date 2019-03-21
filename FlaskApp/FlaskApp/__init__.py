0812acdb662205877ddb1d066cbfcb201dc288ba
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2019-03-21 15:44:49 +0900

    뉴스 요약 API 추가 시도

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -3,6 +3,8 @@ import urllib.request
 from bs4 import BeautifulSoup
 import pymysql
 import requests
+from gensim.summarization import summarize
+from newspaper import Article
 
 app = Flask(__name__)
 
@@ -23,11 +25,12 @@ def Scrape_DB(scraped_Data):
 	db.commit()
 	db.close()
 
-# 웹 크롤링 수행
+
 def scrapeData():
 	headLine_Title = []
 	headLine_Url = []
-
+	headLine_Summary = []
+	
 	url = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
 	request = requests.get(url)
 	soup = BeautifulSoup(request.content, "lxml")
@@ -35,8 +38,18 @@ def scrapeData():
 	for element in data:
 		headLine_Title.append(element.text)
 		headLine_Url.append(element.get('href'))
-	for i in range(len(headLine_Title)):
-		print(headLine_Title[i], ": ", headLine_Url[i])
+
+	for article_url in headLine_Url:
+		news = Article(article_url, language='ko')
+		news.download()
+		news.parse()
+		headLine_Summary.append(summarize(news.text, word_count=50))
+
+	for i in range(len(headLine_Url)):
+		print(headLine_Title[i])
+		print(headLine_Url[i])
+		print(headLine_Summary[i])
+
 
 
 if(__name__ == 'main'):
