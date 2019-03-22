ed5996068a8a2903d2d6b5e90bc89daaa987912f
DESKTOP-4SHJTLB\Joshua Y. S. Jung <joshuanpedia@gmail.com>
2019-03-22 14:56:55 +0900

    테스트용 파이썬 파일 추가 (한국어 패키지)

---
+++ Konlpy.py
@@ -0,0 +1,6 @@
+from konlpy.tag import Komoran
+komoran = Komoran()
+
+
+
+print(komoran.morphs(u'우왕 코모란도 오픈소스가 되었어요'))
---
+++ Scraping.py
@@ -0,0 +1,35 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import pymysql
+import requests
+from gensim.summarization import summarize
+from newspaper import Article
+from konlpy.tag import Komoran
+
+komoran = Komoran()
+def scrapeData():
+    headLine_Title = []
+    headLine_Url = []
+    headLine_Summary = []
+
+    url = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
+    request = requests.get(url)
+    soup = BeautifulSoup(request.content, "lxml")
+    data = soup.find_all('a', {'class': 'cluster_text_headline nclicks(cls_eco.clsart)'})
+    for element in data:
+        headLine_Title.append(element.text)
+        headLine_Url.append(element.get('href'))
+    '''
+    for article_url in headLine_Url:
+        news = Article(article_url, language='ko')
+        news.download()
+        news.parse()
+        headLine_Summary.append(summarize(news.text, word_count=50))
+    '''
+    return headLine_Title
+
+temp = scrapeData()
+
+for title in temp:
+    print(komoran.morphs(title))
+
