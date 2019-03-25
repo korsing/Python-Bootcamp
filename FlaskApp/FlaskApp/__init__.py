36daccededfdb43f854d6e7cb1ba6495f92ffd60
MXNEY_ELO <mxneyelo@gmail.com>
2019-03-25 11:38:19 +0900

    HTML TEST

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -6,44 +6,45 @@ import pymysql
 import requests
 from gensim.summarization import summarize
 from newspaper import Article
+import datetime ########################################################수정
 
 app = Flask(__name__)
 
 # 전체보기 (홈화면)
+
+##JS작업 교체부분############################################################
+import datetime
+
+year = datetime.datetime.now().year
+month = datetime.datetime.now().month
+day = datetime.datetime.now().date().day
+today = year+'. '+month+'. '+day
+
+####table 1#####
+x=7;
+num = 0;
+companyNames = ["(주)LG화학","S-OIL(주)","에스케이네트웍스(주)","에스케이하이닉스(주)","현대모비스(주)","삼성화재해상보험(주)","한국토지주택공사","현대중공업(주)","한국가스공사",
+				"(주)포스코","엘지디스플레이(주)","삼성디스플레이(주)","GS칼텍스(주)","삼성생명보험(주)","SK에너지(주)","LG전자(주)","기아자동차(주)","현대자동차(주)","한국전력공사","삼성전자(주)","한국전력공사"];
+keywords = [['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3'],
+			['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3'],
+			['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3'],
+			['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3']];
+count = [[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],
+		[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7]];
+
+
+############################################################################
 @app.route('/')
 def homepage():
-	title = []
-	url = []
-	summary = []
+	#title, url, summary = scrapeData()
+	title=[1,2,3];url=[4,5,6];summary=[7,8,9]
+	article_count = len(title)
+	return render_template('index.html', title=title,url=url,summary=summary,count=article_count)
 
-	# 네이버 경제 뉴스 URL
-	url = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
-
-	request = requests.get(url)
-	soup = BeautifulSoup(request.content, "lxml")
-	data = soup.find_all('a', {'class': 'cluster_text_headline nclicks(cls_eco.clsart)'})
-	
-	# Scrape된 헤드라인 데이터 중 text 값은 Title로, href 태그는 URL로 저장
-	for element in data:
-		titles.append(element.text)
-		article_url = element.get('href')
-		urls.append(article_url)
-		# newspaper 라이브러리로 기사 본문 발췌
-		news = Article(article_url, language='ko')
-		news.download()
-		news.parse()
-		# summarize 함수로 50자로 요약 진행
-		summarys.append(summarize(news.text, word_count=50))
-
-	
-	article_count = len(titles)
-	return render_template('index.html', title=titles,url=urls,summary=summarys,count=article_count)
-
-'''
 def scrapeData(url):
-	titles = []
-	urls = []
-	summarys = []
+	headLine_Title = []
+	headLine_Url = []
+	headLine_Summary = []
 
 	# 네이버 경제 뉴스 URL
 	url = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
@@ -51,28 +52,29 @@ def scrapeData(url):
 	request = requests.get(url)
 	soup = BeautifulSoup(request.content, "lxml")
 	data = soup.find_all('a', {'class': 'cluster_text_headline nclicks(cls_eco.clsart)'})
-	
+
 	# Scrape된 헤드라인 데이터 중 text 값은 Title로, href 태그는 URL로 저장
 	for element in data:
-		titles.append(element.text)
+		headLine_Title.append(element.text)
 		article_url = element.get('href')
-		urls.append(article_url)
+		headLine_Url.append(article_url)
 		# newspaper 라이브러리로 기사 본문 발췌
 		news = Article(article_url, language='ko')
 		news.download()
 		news.parse()
 		# summarize 함수로 50자로 요약 진행
-		summarys.append(summarize(news.text, word_count=50))
-	
-	return titles, urls, summarys
+		headLine_Summary.append(summarize(news.text, word_count=50))
+
+	return headLine_Title, headLine_Url, headLine_Summary
 
 if(__name__ == 'main'):
 	app.run()
 
+'''
 def connectDB():
 	db = pymysql.connect(host="localhost", user="root", passwd="skgkdlslrtm", db="Bootcamp", charset='utf8')
 	cur = db.cursor()
-	# 현재 
+	# 현재
 	cur.execute("SELECT max(seq) from scrape;")
 	maxseq = cur.fetchone()[0]
 	if(maxseq != None):
