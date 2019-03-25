7cccf3c38619bca7415dd0979faccbc546625b09
Joshua Y. S. Jung <joshuanpedia@gmail.com>
2019-03-25 09:04:49 +0900

    메인 실행 함수에 옮겨보기

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -12,15 +12,9 @@ app = Flask(__name__)
 # 전체보기 (홈화면)
 @app.route('/')
 def homepage():
-	title, url, summary = scrapeData()
-	#title=[1,2,3];url=[4,5,6];summary=[7,8,9]
-	article_count = len(title)
-	return render_template('index.html', title=title,url=url,summary=summary,count=article_count)
-
-def scrapeData(url):
-	headLine_Title = []
-	headLine_Url = []
-	headLine_Summary = []
+	title = []
+	url = []
+	summary = []
 
 	# 네이버 경제 뉴스 URL
 	url = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
@@ -31,22 +25,50 @@ def scrapeData(url):
 	
 	# Scrape된 헤드라인 데이터 중 text 값은 Title로, href 태그는 URL로 저장
 	for element in data:
-		headLine_Title.append(element.text)
+		titles.append(element.text)
 		article_url = element.get('href')
-		headLine_Url.append(article_url)
+		urls.append(article_url)
 		# newspaper 라이브러리로 기사 본문 발췌
 		news = Article(article_url, language='ko')
 		news.download()
 		news.parse()
 		# summarize 함수로 50자로 요약 진행
-		headLine_Summary.append(summarize(news.text, word_count=50))
+		summarys.append(summarize(news.text, word_count=50))
+
 	
-	return headLine_Title, headLine_Url, headLine_Summary
+	article_count = len(titles)
+	return render_template('index.html', title=titles,url=urls,summary=summarys,count=article_count)
+
+'''
+def scrapeData(url):
+	titles = []
+	urls = []
+	summarys = []
+
+	# 네이버 경제 뉴스 URL
+	url = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
+
+	request = requests.get(url)
+	soup = BeautifulSoup(request.content, "lxml")
+	data = soup.find_all('a', {'class': 'cluster_text_headline nclicks(cls_eco.clsart)'})
+	
+	# Scrape된 헤드라인 데이터 중 text 값은 Title로, href 태그는 URL로 저장
+	for element in data:
+		titles.append(element.text)
+		article_url = element.get('href')
+		urls.append(article_url)
+		# newspaper 라이브러리로 기사 본문 발췌
+		news = Article(article_url, language='ko')
+		news.download()
+		news.parse()
+		# summarize 함수로 50자로 요약 진행
+		summarys.append(summarize(news.text, word_count=50))
+	
+	return titles, urls, summarys
 
 if(__name__ == 'main'):
 	app.run()
 
-'''
 def connectDB():
 	db = pymysql.connect(host="localhost", user="root", passwd="skgkdlslrtm", db="Bootcamp", charset='utf8')
 	cur = db.cursor()
