08775ca4302c0c38b9dfa89f04aba8673f34f6da
MXNEY_ELO <mxneyelo@gmail.com>
2019-03-25 11:56:51 +0900

    젭알

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -18,7 +18,7 @@ import datetime
 year = datetime.datetime.now().year
 month = datetime.datetime.now().month
 day = datetime.datetime.now().date().day
-today = year+'. '+month+'. '+day
+today = str(year)+'. '+str(month)+'. '+str(day)
 
 ####table 1#####
 x=7;
