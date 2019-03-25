34e209619dfb77f3904bc54b9f137079b9018fef
gustav1234 <gustav1234@naver.com>
2019-03-25 16:49:43 +0900

    companylist db

---
+++ FlaskApp/FlaskApp/companylistdb.py
@@ -0,0 +1,20 @@
+import csv
+#import pymysql
+import mysql.connector
+
+mydb = mysql.connector.connect(host='localhost',
+    user='root',
+    passwd='skgkdlslrtm',
+    db='Bootcamp',
+    charset='utf8')
+cursor = mydb.cursor()
+
+csv_data = csv.reader(file('datalist.csv'))
+for row in csv_data:
+
+    cursor.execute('INSERT INTO company(code, names)' 
+          'VALUES("%s", "%s")',
+          row)
+#close the connection to the database.
+mydb.commit()
+cursor.close()
