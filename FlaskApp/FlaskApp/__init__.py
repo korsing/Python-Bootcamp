05ba63850e2a2c2ec3b0fa9c63790382f0c22626
Joshua Y. S. Jung <joshuanpedia@gmail.com>
2019-03-26 11:02:56 +0900

    크롤링 네이버 경제, 주식 제발...

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -15,31 +15,9 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-x=7;
-num = 0;
-companyNames = ["(주)LG화학","S-OIL(주)","에스케이네트웍스(주)","에스케이하이닉스(주)","현대모비스(주)","삼성화재해상보험(주)","한국토지주택공사","현대중공업(주)","한국가스공사",
-				"(주)포스코","엘지디스플레이(주)","삼성디스플레이(주)","GS칼텍스(주)","삼성생명보험(주)","SK에너지(주)","LG전자(주)","기아자동차(주)","현대자동차(주)","한국전력공사","삼성전자(주)","한국전력공사"];
-keywords = [['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3'],
-			['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3'],
-			['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3'],
-			['키워드1_1','키워드1_2','키워드1_3'],['키워드2_1','키워드2_2','키워드2_3'],['키워드3_1','키워드3_2','키워드3_3'],['키워드4_1','키워드4_2','키워드4_3'],['키워드5_1','키워드5_2','키워드5_3'],['키워드6_1','키워드6_2','키워드6_3']];
-count = [[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7],
-		[1,2,3],[3,4,5],[2,3,4],[3,1,4],[7,3,7],[8,4,7]];
-compNum = len(companyNames)
-####table 2#####
-UserNames = ["홍길동","고길동","한수위","Jason","정주용","Peter","최재혁","김날수"];
-OnClick = ["한국가스공사","(주)포스코","엘지디스플레이(주)","삼성디스플레이(주)","GS칼텍스(주)","삼성생명보험(주)","SK에너지(주)","LG전자(주)"];
-####table 3#####
-keywords_ranked = ["키워드랭크1","키워드랭크2","키워드랭크3","키워드랭크4","키워드랭크5","키워드랭크6","키워드랭크7","키워드랭크8","키워드랭크9","키워드랭크10"];
-ranked_count = [15,12,11,10,8,7,5,4,3,2]
-
-
 @app.route('/')
 def homepage():
 
-	date = today()
-	title=[1,2,3];url=[4,5,6];summary=[7,8,9]
-	article_count = len(title)
 	return render_template('index.html', date = date, companyNames = companyNames, compNum = compNum, keywords = keywords, count = count,
 							UserNames = UserNames, OnClick = OnClick, keywords_ranked = keywords_ranked, ranked_count = ranked_count)
 	#return render_template('index.html', title=title,url=url,summary=summary,count=article_count)
@@ -66,23 +44,35 @@ def today():
 	return year, month, day
 
 def scrapeData():
-	headLine_Title = []
-	headLine_Url = []
+	# NAVER SCRAPYING
+	
+	headline_Naver = []
+	url_Naver = []
 
 	# 네이버 경제 뉴스 URL
-	url = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
+	url_Economy = 'https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101'
+	url_Stock = 'https://finance.naver.com/news/news_list.nhn?mode=LSS3D&section_id=101&section_id2=258&section_id3=402'
 
-	request = requests.get(url)
-	soup = BeautifulSoup(request.content, "lxml")
-	data = soup.find_all('a', {'class': 'cluster_text_headline nclicks(cls_eco.clsart)'})
+	req_Economy = requests.get(url_Economy)
+	req_Stock = requests.get(url_Stock)
 
-	# Scrape된 헤드라인 데이터 중 text 값은 Title로, href 태그는 URL로 저장
-	for element in data:
-		headLine_Title.append(element.text)
-		article_url = element.get('href')
-		headLine_Url.append(article_url)
-	return headLine_Title, headLine_Url
+	soup_Economy = BeautifulSoup(req_Economy.content, "lxml")
+	soup_Stock = BeautifulSoup(req_Stock.content, "lxml")
 
+	headlines_Economy = soup_Economy.find_all('a', {'class': 'cluster_text_headline nclicks(cls_eco.clsart)'})
+	headlines_Stock = soup_Stock.find_all('dd',{'class':'articleSubject'})
+
+	for title in headlines_Economy:
+		headline_Naver.append(title.text)
+		url_Naver.append(title.get('href'))
+	for title in headlines_Stock:
+		headline_Naver.append(title.text)
+		url_Naver.append('https://finance.naver.com' + title.find('a').get('href'))
+	
+	# URL 던져주면 요약해주는 함수 실행
+	
+	return headline_Naver, url_Naver
+# Main
 if(__name__ == 'main'):
 	app.run()
 
