93285ef73836d03921a767e12464530a6440ab21
Joshua Y. S. Jung <joshuanpedia@gmail.com>
2019-03-27 15:24:02 +0900

    뭐가문제여

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -3,6 +3,7 @@
 # Flask related modules
 from flask import Flask, render_template, session, redirect, flash, request
 
+'''
 # DB related modules
 import pymysql
 # Date and Time related modules
@@ -23,18 +24,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from konlpy.tag import Komoran
 from collections import Counter
 import numpy as np
-
+'''
 # 전역변수 선언
 app = Flask(__name__)
-komoran = Komoran()
+# komoran = Komoran()
 
 @app.route('/')
 def homepage():
 	return "DB WORKING!"
-	#return render_template('index.html', date = date, companyNames = companyNames, compNum = compNum, keywords = keywords, count = count,
-	#						UserNames = UserNames, OnClick = OnClick, keywords_ranked = keywords_ranked, ranked_count = ranked_count)
-	#return render_template('index.html', title=title,url=url,summary=summary,count=article_count)
-
 
 # Main
 if(__name__ == 'main'):
